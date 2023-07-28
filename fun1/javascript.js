98179236eedd6c38173ea622f2de69ec0bd09335
Benjamin Gearhart <benjamingearhart@gmail.com>
2023-07-28 16:14:32 -0600

    fundamentals 3 complete

--- fun1/javascript.js
+++ fun1/javascript.js
@@ -1,12 +1,12 @@
-function add7(num){
-  return num + 10
-}
-console.log(add7(7));
+// function add7(num){
+//   return num + 10
+// }
+// console.log(add7(7));
 
-function multiply(num1 , num2){
-  return num1 * num2
-}
-console.log(multiply(2 , 4));
+// function multiply(num1 , num2){
+//   return num1 * num2
+// }
+// console.log(multiply(2 , 4));
 
 // Make the function capitalize the first letter of a string
 function capitalize(str) {
@@ -14,6 +14,19 @@ function capitalize(str) {
     return str;
   }
   const words = str.split(" ");
-  const capotalizedWords = [];
-  
-console.log(capitalize("bean is a perfectly behaved dog"));
+  const capitalizedWords = [];
+
+for (let word of words) {
+  const capitalizedWord = word.charAt(0).toUpperCase() + word.slice(1);
+  capitalizedWords.push(capitalizedWord);
+} 
+const capitalizedStr = capitalizedWords.join(" ");
+return capitalizedStr;
+}
+console.log(capitalize("bean is a perfectly behaved dog and I love him"));
+
+// Write a function called lastLetter that takes a string and returns the very last letter of that string:
+function lastLetter(str) {
+  return str.charAt(str.length - 1);
+}
+console.log(lastLetter("beaner"));
