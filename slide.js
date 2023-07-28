39a53addfeb5e88679080781a22c2ceff3b4c99c
Benjamin Gearhart <benjamingearhart@gmail.com>
2023-07-27 18:34:48 -0600

    fundamentals 3

--- slide.js
+++ slide.js
@@ -1,5 +1,6 @@
-$('.slide a').click(function () {
-    $('.slide.active').removeClass('active');
-    $(this).closest('.slide').addClass('active');
-    return false;
-  });
+$('.slide a').click(function (event) {
+  event.preventDefault(); // Prevents the default action of following the link
+  
+  $('.slide.active').removeClass('active');
+  $(this).closest('.slide').addClass('active');
+});
